dc5575364a10b08970f787f3ee033b03b78efb47
Adam L. Taylor <adamlyletaylor@gmail.com>
2022-06-14 13:28:49 -0400

    python executable now determined by conda environment

--- transfero_launcher.sh
+++ transfero_launcher.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 escaped_bash_profile_path=${1}
-python_executable_path=${2}
-transfero_script_path=${3}
-cluster_billing_account_name=${4}
-escaped_transfero_logs_folder_path=${5}
+transfero_script_path=${2}
+cluster_billing_account_name=${3}
+escaped_transfero_logs_folder_path=${4}
 date_as_string=`date +%Y-%m-%d`
 transfero_log_file_name="transfero-${date_as_string}.log"
 transfero_log_file_path="${escaped_transfero_logs_folder_path}/${transfero_log_file_name}"
