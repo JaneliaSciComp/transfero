1279a5b57e8bb4dd038f6df3f3b0d8c8e3271dc0
Adam L. Taylor <adamlyletaylor@gmail.com>
2022-09-01 13:57:23 -0400

    Changed copy_transfero_into_production.py to copy_into_production.py, which leans heavily on general functions in tpt

---
+++ copy_into_production.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import os
+import tempfile
+import stat
+
+from tpt.utilities import *
+
+
+
+def main() :
+    url = 'https://github.com/JaneliaSciComp/transfero'
+    username_from_user_index = [ 'asorobot' ]
+    clone_and_copy_github_repository_into_user_home_folders(url, username_from_user_index)
+
+
+
+if __name__ == "__main__":
+    main()
