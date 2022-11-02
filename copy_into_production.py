caeefe57cac46ffc7b48d5e306100e255ccd56f3
Adam L. Taylor <taylora@janelia.hhmi.org>
2022-11-02 16:51:48 -0400

    Added configs for more labs, updated copy_into_production.py, got rid of unused does_use_per_user_folders field in config files

--- copy_into_production.py
+++ copy_into_production.py
@@ -10,7 +10,7 @@ from tpt.utilities import *
 
 def main() :
     url = 'https://github.com/JaneliaSciComp/transfero'
-    username_from_user_index = [ 'asorobot' ]
+    username_from_user_index = [ 'asorobot', 'rubinlab', 'bransonlab', 'projtechreslab', 'geniegeneric' ]
     clone_and_copy_github_repository_into_user_home_folders(url, username_from_user_index)
 
 
