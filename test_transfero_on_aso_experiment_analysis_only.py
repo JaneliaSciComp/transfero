b4ca7dd931598005a691924dcddb94635cf8184b
Adam L. Taylor <taylora@janelia.hhmi.org>
2022-11-15 18:18:41 -0500

    Added test_transfero_on_aso_experiment_analysis_only.py

--- test_transfero_on_aso_experiment_analysis_only.py
+++ test_transfero_on_aso_experiment_analysis_only.py
@@ -2,15 +2,14 @@
 
 import os
 import itertools
-
 from transfero import *
 
 
 
 def check_for_aso_pipeline_output_files(relative_path_to_folder_from_experiment_index, transfero_destination_folder_path) :
     # This function prints stuff to stdout when there are test failures
-    successful_test_file_names = { 'movie.ufmf.flytracked/movie-track.mat', 'movie.ufmf.flytracked/movie-feat.mat', 'movie.ufmf.flytracked/movie-calibration.mat' } 
-    failed_test_file_names = {} 
+    successful_test_file_names = [ 'movie.ufmf.flytracked/movie-track.mat', 'movie.ufmf.flytracked/movie-feat.mat', 'movie.ufmf.flytracked/movie-calibration.mat' ] 
+    failed_test_file_names = [] 
     experiment_count = len(relative_path_to_folder_from_experiment_index) 
     all_tests_passed_from_experiment_index = [True] * len(relative_path_to_folder_from_experiment_index)
     for experiment_index in range(experiment_count) :
@@ -18,21 +17,21 @@ def check_for_aso_pipeline_output_files(relative_path_to_folder_from_experiment_
         experiment_folder_path = os.path.join(transfero_destination_folder_path, relative_path_to_experiment_folder) 
         # First check for the file that indicates we wouldn't expect a experiment to
         # process fully
-        is_experiment_expected_to_succeed = os.path.exists(os.path.join(experiment_folder_path, 'aso-pipeline-should-fail-for-this-experiment'))
+        is_experiment_expected_to_succeed = not os.path.exists(os.path.join(experiment_folder_path, 'aso-pipeline-should-fail-for-this-experiment'))
         if is_experiment_expected_to_succeed :
             for i in range(len(successful_test_file_names)) :
                 test_file_name = successful_test_file_names[i] 
                 test_file_path = os.path.join(experiment_folder_path, test_file_name) 
                 if not os.path.exists(test_file_path) :
-                    printfe('Test failure: No output file at %s\n' % test_file_path) 
+                    printf('Test failure: No output file at %s\n' % test_file_path) 
                     all_tests_passed_from_experiment_index[experiment_index] = False 
         else :
             # Check for the files that *should* be there
-            for i in range(failed_test_file_names) :
+            for i in range(len(failed_test_file_names)) :
                 test_file_name = failed_test_file_names[i]
                 test_file_path = os.path.join(experiment_folder_path, test_file_name) 
                 if not os.path.exists(test_file_path) :
-                    printfe('Test failure: No output file at %s\n' % test_file_path) 
+                    printf('Test failure: No output file at %s\n' % test_file_path) 
                     all_tests_passed_from_experiment_index[experiment_index] = False 
     return all_tests_passed_from_experiment_index
 
@@ -43,8 +42,9 @@ def test_transfero_on_aso_rig(do_transfer_data_from_rigs=True, do_run_analysis=T
     this_script_path = os.path.realpath(__file__)
     this_folder_path = os.path.dirname(this_script_path)
     project_folder_path = os.path.dirname(this_folder_path) 
-    root_example_experiments_folder_path = os.path.join(project_folder_path, 'example-experiments') 
-    read_only_example_experiments_folder_path = os.path.join(root_example_experiments_folder_path, 'single-fake-aso-experiment-read-only') 
+    root_example_experiments_folder_path = os.path.join(project_folder_path, 'example-experiments')     
+    read_only_example_experiments_folder_path = os.path.join(root_example_experiments_folder_path, 'single-short-aso-experiment-read-only') 
+    #read_only_example_experiments_folder_path = os.path.join(root_example_experiments_folder_path, 'single-fake-aso-experiment-read-only') 
     #read_only_example_experiments_folder_path = os.path.join(root_example_experiments_folder_path, 'single-katie-experiment-2022-04-05-read-only') 
     #read_only_example_experiments_folder_path = os.path.join(root_example_experiments_folder_path, 'passing-test-suite-experiments-read-only') 
     #read_only_example_experiments_folder_path = os.path.join(root_example_experiments_folder_path, 'single-passing-test-suite-experiment-with-tracking-read-only') 
@@ -70,6 +70,8 @@ def test_transfero_on_aso_rig(do_transfer_data_from_rigs=True, do_run_analysis=T
     per_lab_configuration['analysis_executable_path'] = '../aso-deepmind-experiment-analysis-pipeline/aso_deepmind_experiment_analysis_pipeline.py'  # this is interpreted realtive to folder containing transfero.py
     per_lab_configuration['slots_per_analysis_job'] = 48
     per_lab_configuration['maximum_analysis_slot_count'] = 480
+    per_lab_configuration['user_name_for_configuration_purposes'] = 'asorobot'
+    per_lab_configuration['log_file_name'] = 'transfero-analysis.log'
 
     # Get the relative paths of all the experiment folders
     absolute_path_to_read_only_folder_from_experiment_index = find_experiment_folders(read_only_example_experiments_folder_path) 
@@ -113,17 +115,27 @@ def test_transfero_on_aso_rig(do_transfer_data_from_rigs=True, do_run_analysis=T
     # Run transfero
     #analysis_parameters = { 'doautomaticcheckscomplete', 'on' } 
     print('Running transfero...') 
-    transfero(do_transfer_data_from_rigs, do_run_analysis, per_lab_configuration)
+    (relative_path_from_synched_experiment_folder_index, job_status_from_experiment_index) = \
+        transfero(do_transfer_data_from_rigs, do_run_analysis, per_lab_configuration)
 
     # Check that the expected files are present on dm11
     local_verify(read_only_example_experiments_folder_path, transfero_destination_folder_path) 
+
+    # Check the return values from transfero()
+    if len(relative_path_from_synched_experiment_folder_index)==0 and len(job_status_from_experiment_index)==1 and all(job_status==+1 for job_status in job_status_from_experiment_index) :
+        # all is well
+        pass
+    else :
+        print('relative_path_from_synched_experiment_folder_index: ', relative_path_from_synched_experiment_folder_index)
+        print('job_status_from_experiment_index: ', job_status_from_experiment_index)
+        raise RuntimeError('It seems transfero had issues')
 
     # Check that some of the expected outputs were generated
     all_tests_passed_from_experiment_index = check_for_aso_pipeline_output_files(relative_path_to_folder_from_experiment_index, transfero_destination_folder_path) 
     if all(all_tests_passed_from_experiment_index) :
         print('All experiment folder checks pass at 1st check, except those that were expected not to pass.\n') 
     else :
-        some_tests_failed_from_experiment_index = map(@not, all_tests_passed_from_experiment_index) ;
+        some_tests_failed_from_experiment_index = map(lambda x: (not x), all_tests_passed_from_experiment_index) ;
         relative_path_to_folder_from_failed_experiment_index = \
             itertools.compress(relative_path_to_folder_from_experiment_index, some_tests_failed_from_experiment_index)
         print("Experiment(s) with problems at first check:\n")
@@ -142,22 +154,31 @@ def test_transfero_on_aso_rig(do_transfer_data_from_rigs=True, do_run_analysis=T
                                (rig_host_name, rig_data_folder_path, experiment_folder_count))
 
     # Run transfero again, make sure nothing has changed
-    transfero(do_transfer_data_from_rigs, do_run_analysis, per_lab_configuration)         
+    (relative_path_from_synched_experiment_folder_index, job_status_from_experiment_index) = \
+        transfero(do_transfer_data_from_rigs, do_run_analysis, per_lab_configuration)         
 
     # Check that the expected files are present on dm11
     local_verify(read_only_example_experiments_folder_path, transfero_destination_folder_path) 
 
+    # Check the return values from transfero()
+    # Both should be empty b/c there was nothing to do on the re-run
+    if len(relative_path_from_synched_experiment_folder_index)==0 and len(job_status_from_experiment_index)==0 :
+        # all is well
+        pass
+    else :
+        print('relative_path_from_synched_experiment_folder_index: ', relative_path_from_synched_experiment_folder_index)
+        print('job_status_from_experiment_index: ', job_status_from_experiment_index)
+        raise RuntimeError('It seems transfero had issues')
+
     # Check that some of the expected outputs were generated
     all_tests_passed_from_experiment_index = check_for_aso_pipeline_output_files(relative_path_to_folder_from_experiment_index, transfero_destination_folder_path) 
     if all(all_tests_passed_from_experiment_index) :
-        print('All experiment folder checks pass at 1st check, except those that were expected not to pass.\n') 
+        print('All experimental folder checks pass at 2nd check, except those that were expected not to pass.\n') 
     else :
-        some_tests_failed_from_experiment_index = map(@not, all_tests_passed_from_experiment_index) ;
+        some_tests_failed_from_experiment_index = map(lambda x: (not x), all_tests_passed_from_experiment_index)
         relative_path_to_folder_from_failed_experiment_index = \
             itertools.compress(relative_path_to_folder_from_experiment_index, some_tests_failed_from_experiment_index)
-        print("Experiment(s) with problems at first check:\n")
-        map(lambda path: print("  %s\n", path), relative_path_to_folder_from_failed_experiment_index)
-        raise Exception('Some experiments had problems at 2nd check') 
+        raise Exception('Some experiments had problems at 2nd check')
 
     # Check that the rig lab folder is empty now
     if do_transfer_data_from_rigs :
@@ -178,4 +199,4 @@ def test_transfero_on_aso_rig(do_transfer_data_from_rigs=True, do_run_analysis=T
 
 
 if __name__ == "__main__":
-    test_transfero_on_aso_rig(True, True)
+    test_transfero_on_aso_rig(False, True)
