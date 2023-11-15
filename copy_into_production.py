3f350d18b26fd140c7a2b20eef02b0c35cb69c52
Adam L. Taylor <taylora@janelia.hhmi.org>
2023-11-15 17:22:44 -0500

    main: Added Otopalik Lab configuration

--- copy_into_production.py
+++ copy_into_production.py
@@ -10,7 +10,7 @@ from tpt.utilities import *
 
 def main() :
     url = 'https://github.com/JaneliaSciComp/transfero'
-    username_from_user_index = [ 'asorobot', 'rubinlab', 'bransonlab', 'projtechreslab', 'geniegeneric' ]
+    username_from_user_index = [ 'asorobot', 'rubinlab', 'bransonlab', 'projtechreslab', 'geniegeneric', 'otopalikrobot' ]
     clone_and_copy_github_repository_into_user_home_folders(url, username_from_user_index)
 
 
